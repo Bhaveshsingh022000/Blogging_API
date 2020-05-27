ddc225f04ab710c6fcad02289cd200c1f3ab42cb
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-27 19:05:36 +0530

    improved project structure

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { body } = require('express-validator/check');
+const User = require('../models/user');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.put('/signup',[
+    body('email').isEmail()
+    .withMessage('Please enert valid email')
+    .custom((value, {req}) =>{
+        return User.findOne({email: value}).then(userDoc => {
+            if(userDoc){
+            return Promise.reject("Email alerdy exist");
+            }
+        });
+    }).normalizeEmail(),
+    body('password').trim().isLength({min:5}),
+    body('name').trim().not().isEmpty()
+], authController.signup);
+
+router.post('/login', authController.login);
+
+module.exports = router;
